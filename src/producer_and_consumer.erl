ff37a7fa95dee95e469875e076332c3c8e9b1bb2
Erico de Souza Loewe <ed-lowe@hotmail.com>
2019-06-08 19:38:52 -0300

    feat(main): add args

--- src/producer_and_consumer.erl
+++ src/producer_and_consumer.erl
@@ -1,13 +1,35 @@
 -module(producer_and_consumer).
 
 %% API exports
--export([main/0]).
+-export([main/0, main/1]).
 
 %% escript Entry point
-main() ->
+main() -> main(10, 10).
+
+main(Args) ->
+    io:format("[main] started with args ~p ~n", [Args]),
+    main().
+
+main(NumberOfConsumers, NumberOfProducers) ->
     io:format("[main] started"),
     BufferPid = spawn(buffer, start, []),
-    ConsumerPid = spawn(consumer, start, [1, BufferPid]),
-    ProducerPid = spawn(producer, start, [1, BufferPid]),
-    io:format("[main] PIDS => [~p,~p,~p]",
-	      [BufferPid, ProducerPid, ConsumerPid]).
+    createConsumers(NumberOfConsumers, BufferPid),
+    createProducers(NumberOfProducers, BufferPid).
+
+createConsumers(NumberOfConsumers, BufferPid) ->
+    spawn(fun () ->
+		  lists:foreach(fun (Index) ->
+					spawn(consumer, start,
+					      [Index, BufferPid])
+				end,
+				lists:seq(1, NumberOfConsumers))
+	  end).
+
+createProducers(NumberOfProducers, BufferPid) ->
+    spawn(fun () ->
+		  lists:foreach(fun (Index) ->
+					spawn(producer, start,
+					      [Index, BufferPid])
+				end,
+				lists:seq(1, NumberOfProducers))
+	  end).
