357b903f5b13e011d5db0314aecbd1e81b85dd12
Erico de Souza Loewe <ed-lowe@hotmail.com>
2019-06-08 18:45:03 -0300

    feat(consumer): add consumer and producer logic

--- src/buffer.erl
+++ src/buffer.erl
@@ -7,13 +7,42 @@ start() ->
     io:format("[buffer] started ~n"), manage([], []).
 
 manage(Consumers, Products) ->
+    io:format("[buffer] managing: c=~p p=~p ~n",
+	      [length(Consumers), length(Products)]),
     receive
       {ConsumerId, PID} ->
-	  io:format("[buffer] receive consumer: ~p ~p ~n",
-		    [ConsumerId, PID]),
-	  manage(Consumers ++ [PID], Products);
+	  receiveConsumerAndKeepManaging(ConsumerId, PID,
+					 Consumers, Products);
       {ProducerId, Product, PID} ->
-	  io:format("[buffer] receive producer: ~p ~p ~p ~n",
-		    [ProducerId, Product, PID]),
-	  manage(Consumers, Products ++ [Product])
+	  receiveProductAndKeepManaging(ProducerId, Product, PID,
+					Consumers, Products)
+    end.
+
+notifyConsumerAndKeepManaging(Consumers, Products) ->
+    [PID] = Consumers,
+    [MoreOldProduct] = Products,
+    PID ! {MoreOldProduct},
+    manage(lists:sublist(Consumers, 2, length(Consumers)),
+	   lists:sublist(Products, 2, length(Products))).
+
+receiveConsumerAndKeepManaging(ConsumerId, PID,
+			       Consumers, Products) ->
+    io:format("[buffer] receive consumer: ~p ~p ~n",
+	      [ConsumerId, PID]),
+    NextConsumers = Consumers ++ [PID],
+    if length(Products) == 0 ->
+	   manage(NextConsumers, Products);
+       true ->
+	   notifyConsumerAndKeepManaging(NextConsumers, Products)
+    end.
+
+receiveProductAndKeepManaging(ProducerId, Product, PID,
+			      Consumers, Products) ->
+    io:format("[buffer] receive producer: ~p ~p ~p ~n",
+	      [ProducerId, Product, PID]),
+    NextProducts = Products ++ [Product],
+    if length(Consumers) == 0 ->
+	   manage(Consumers, NextProducts);
+       true ->
+	   notifyConsumerAndKeepManaging(Consumers, NextProducts)
     end.
